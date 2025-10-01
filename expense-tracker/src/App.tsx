6835c6fd6debd4e552e9932b69fc31a51a6dd340
mudasir ahmad <mudassirmwt788@gmail.com>
2025-10-01 07:11:28 +0500

    setup libary and tw ec

---
+++ expense-tracker/src/App.tsx
@@ -0,0 +1,10 @@
+import Navbar from "./myComponents/Navbar"
+
+function App() {
+
+  return (
+    <Navbar/>
+  )
+}
+
+export default App
---
+++ expense-tracker/vite.config.ts
@@ -0,0 +1,16 @@
+import { fileURLToPath } from "url"
+import path, { dirname } from "path"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
